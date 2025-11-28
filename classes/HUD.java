aa19f051e76188b8c6ac804e34a06f9de7790d9a
nularyNasser <nahamed.sveil@gmail.com>
2025-11-28 14:26:11 +0100

    Class de mise à jours de l'écrant

--- classes/HUD.java
+++ classes/HUD.java
@@ -1,12 +1,17 @@
-public class HUD implements Observer{
+package classes;
+
+import interfaces.Observervateur;
+
+
+public class HUD implements Observervateur {
     @Override
     public void update(String eventType, double value) {
         if (eventType.equals("HEALTH")) {
-            System.out.println("[HUD] Point de vie mise à jour : " + value);
+            System.out.println("[classes.HUD] Point de vie mise à jour : " + value);
         }
 
         if (eventType.equals("XP")) {
-            System.out.println(("[HUD] XP mise à jour : " + value));
+            System.out.println(("[classes.HUD] XP mise à jour : " + value));
         }
     }
 }
