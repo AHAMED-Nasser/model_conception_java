475c70f81930c4b290b449ae1080a1c2d7187154
nularyNasser <nahamed.sveil@gmail.com>
2025-11-28 14:25:43 +0100

    Création de la classe principal du jeu et test du code

--- MinecraftGame.java
+++ MinecraftGame.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import classes.HUD;
+import classes.Player;
+import classes.SoundSystem;
 
 public class MinecraftGame {
+    public static void main(String[] args) {
+        Player steve = new Player();
+
+        HUD mainHud = new HUD();
+        SoundSystem son = new SoundSystem();
+
+        steve.registerObserver(mainHud);
+        steve.registerObserver(son);
+
+        System.out.println("--- Début de la partie ---");
+
+        // Steve prend des dégâts de chute
+        System.out.println("\n Steve tombe de haut");
+        steve.setHealth(2);
+
+        // Steve gagne de l'expériences
+        System.out.println("\n Steve mine du charbon de haut");
+        steve.setXP(0.90);
+    }
 }
