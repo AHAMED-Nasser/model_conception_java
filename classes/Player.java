193ab72ae73517e3f6728c40aba9d64eda37a527
nularyNasser <nahamed.sveil@gmail.com>
2025-11-28 14:26:39 +0100

    Classe du joueur

--- classes/Player.java
+++ classes/Player.java
@@ -1,26 +1,29 @@
+package classes;
+
+import interfaces.Observervateur;
+import interfaces.Subject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Player implements Subject {
 
-    private List<Observer> observers = new ArrayList<>();
+    private List<Observervateur> observers = new ArrayList<>();
 
     private int health;
     private double xp;
 
-    @Override
-    public void registerObserver(Observer o) {
+    public void registerObserver(Observervateur o) {
         observers.add(o);
     }
 
-    @Override
-    public void removeObserver(Observer o) {
+    public void removeObserver(Observervateur o) {
         observers.remove(o);
     }
 
     @Override
     public void notifyObservers(String eventType, double value) {
-        for (Observer o : observers) {
+        for (Observervateur o : observers) {
             o.update(eventType, value);
         }
     }
